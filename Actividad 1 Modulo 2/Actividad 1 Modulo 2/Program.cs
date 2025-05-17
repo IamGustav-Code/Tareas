86389e8555b4b0265495fd16d3b00fbe6a8bec82
IamGustav-Code <iamgustav.olivera@gmail.com>
2025-05-16 23:34:25 -0300

    Se cargaron archivos
    Carga de archivos de la actividad 1 del modulo 2

---
+++ Actividad 1 Modulo 2/Actividad 1 Modulo 2/Program.cs
@@ -0,0 +1,86 @@
+﻿// Ejercicio 1
+//int numpar = 2;
+
+
+
+//if ( numpar %2 == 0 )
+//{
+//    Console.WriteLine("El Numero es Par!");
+//}
+//else
+//{
+//    Console.WriteLine("El Numero es Impar!");
+//}
+
+//Ejercicio 2-----------------------------------------------
+
+//int num1 = 0;
+
+//if (num1 < 0  )
+//{
+//    Console.WriteLine("El Numero es Negativo!");
+//}
+//else  if (num1 > 0)
+//{
+//    Console.WriteLine("El Numero es Positivo!");
+//}
+//else if (num1 == 0)
+//{
+//    Console.WriteLine(" El Numero es Cero!");
+//}
+
+// Ejercicio 3------------------------------------
+
+//int edad = 50;
+
+//if (edad >= 18)
+//{
+//    Console.WriteLine("Es Mayor de Edad");
+//}
+//else
+//{
+//    Console.WriteLine("No es Mayor de Edad");
+//}
+
+
+
+
+//ejercicio 5
+
+//int num1  = 35;
+
+
+//if (num1 >= 10 & num1 <=20  )
+//    {
+//    Console.WriteLine(" El Numero esta entre 10 y 20!");
+//}
+//else
+//    {
+//    Console.WriteLine(" El Numero No Esta entre 10 y 20!");
+//}
+
+
+//Ejercicio 6-----------------------------------------------------------
+
+//string Letra = "j";
+
+//if ( )
+//{
+
+//}
+
+// Ejercicio 7: -----------------------------------------------------------
+
+
+//int num = 20;
+
+//if (num % 3 == 0 & num % 5 == 0)
+//{
+//    Console.WriteLine("Se Puede Dividir por 3 y por 5 !!");
+//}
+//else
+//{
+//    Console.WriteLine( "No se Puede Dividir !!");
+//}
+
+//Ejerdcicio 8: -------------------------------------------------------------
